c47f7990ee61d6ffc74e1dbee43f9975b83a6ed5
guiinfo3333 <antonioguilhermeinfo@gmail.com>
2021-06-23 22:06:04 -0500

    subindo uma parada

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -6,10 +6,12 @@ console.log(process.env.PORT)
 module.exports = {
   client: 'pg',
   connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: false
     // host: process.env.HOST,
-    user: process.env.USUARIOBANCO,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
+    // user: process.env.USUARIOBANCO,
+    // password: process.env.PASSWORD,
+    // database: process.env.DATABASE
   },
   migrations: {
     directory: `database/migrations`
